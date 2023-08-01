193fb274b530506421d1d5182dd7fee7fe0c883b
Denys Kleimenov <denys.kleimenov.dev@gmail.com>
2023-08-01 12:21:07 +0300

    Header component was created

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import meals from "../../assets/meals.jpg";
+import { HeaderCartButton } from "../HeaderCartButton/HeaderCartButton";
+import styles from "./Header.module.css";
+
+export const Header: React.FC = () => (
+  <>
+    <header className={styles.header}>
+      <h1>Meals Delivery</h1>
+      <HeaderCartButton />
+    </header>
+    <div className={styles["main-image"]}>
+      <img src={meals} alt="A table full of delicious food" />
+    </div>
+  </>
+);
