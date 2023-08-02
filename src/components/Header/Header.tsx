d55cef44f5b1447474c161affa9ffc6e83cf42eb
Denys Kleimenov <denys.kleimenov.dev@gmail.com>
2023-08-02 11:58:04 +0300

    HeaderCartButton component was modified

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,15 @@ import meals from "../../assets/meals.jpg";
 import { HeaderCartButton } from "./HeaderCartButton/HeaderCartButton";
 import styles from "./Header.module.css";
 
-export const Header: React.FC = () => (
+type Props = {
+  handleOpenCart: () => void;
+};
+
+export const Header: React.FC<Props> = ({ handleOpenCart }) => (
   <>
     <header className={styles.header}>
       <h1>Meals Delivery</h1>
-      <HeaderCartButton />
+      <HeaderCartButton handleClick={handleOpenCart} />
     </header>
     <div className={styles["main-image"]}>
       <img src={meals} alt="A table full of delicious food" />
--- src/components/Header/HeaderCartButton/HeaderCartButton.tsx
+++ src/components/Header/HeaderCartButton/HeaderCartButton.tsx
@@ -1,12 +1,42 @@
 import styles from "./HeaderCartButton.module.css";
 import { CartIcon } from "../../CartIcon";
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../../../store/CartContext";
 
-export const HeaderCartButton: React.FC = () => (
-  <button className={styles.button}>
-    <span className={styles.icon}>
-      <CartIcon />
-    </span>
-    <span>Your cart</span>
-    <span className={styles.badge}>3</span>
-  </button>
-);
+type Props = {
+  handleClick: () => void;
+};
+
+export const HeaderCartButton: React.FC<Props> = ({ handleClick }) => {
+  const { items } = useContext(CartContext);
+  const [isAnimationActive, setIsAnimationActive] = useState(false);
+  const numberOfItems = items.reduce((prev, curr) => prev + curr.amount, 0);
+  
+  useEffect(() => {
+    if (!items.length) {
+      return;
+    }
+
+    setIsAnimationActive(true);
+
+    const timerId = setTimeout(() => {
+      setIsAnimationActive(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [items])
+
+  const buttonStyles = `${styles.button} ${isAnimationActive ? styles.bump : ''}`
+
+  return (
+    <button className={buttonStyles} onClick={handleClick}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span>Your cart</span>
+      <span className={styles.badge}>{numberOfItems}</span>
+    </button>
+  );
+};
